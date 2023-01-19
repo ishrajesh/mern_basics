e445280f6fb79ab28b9e5687eb45ed2acea92c12
ishrajesh <ishrajeshshelley@gmail.com>
2023-01-19 17:11:22 +0530

    Adding errorHandler middleware

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -3,6 +3,7 @@
 // @access  private
 
 const getGoals = (req,res) => { 
+    
     res.status(200).json({msg:'Get goals'})
 }
 
@@ -11,6 +12,11 @@ const getGoals = (req,res) => {
 // @access  private
 
 const setGoal = (req,res) => { 
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field');
+
+    }
     res.status(200).json({msg:'Create goal'})
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const { application } = require('express');
 const express = require('express');
+const {errorHandler} = require('./middleware/errorMiddleware')
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+// Midddleware to use body content of req
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
 app.use('/api/goals',require('./routes/goalRoutes'));
+
+app.use(errorHandler);
 
 app.listen(port, ()=> console.log(`Server stated at PORT: ${port}  ...`))
