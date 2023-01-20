105f7bb9af1c448dc53442f1559d7023e6dca71a
ishrajesh <ishrajeshshelley@gmail.com>
2023-01-20 17:30:09 +0530

    REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const { application } = require('express');
 const express = require('express');
-const {errorHandler} = require('./middleware/errorMiddleware')
+const colors = require('colors');
 const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+connectDB();
 // Midddleware to use body content of req
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
