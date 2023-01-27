05f8435cc94f9ea7f1faa51bf57a8edd6430e29c
ishrajesh <ishrajeshshelley@gmail.com>
2023-01-27 17:45:02 +0530

    protect getMe route and send details in response

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,25 +3,26 @@ const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 
 
+
 const protect = asyncHandler(async (req,res,next)=>{
     let token
 
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
         try{
             //Get token from header
-            token = req.header.authorization.split(' ')[1]
+            token = req.headers.authorization.split(' ')[1]
 
             // Verify token and decode it
             const decode = jwt.verify(token, process.env.JWT_SECRET)
 
             // Get user fom the token
-            req.user = await User.findById(decoded.id).select('-password')
+            req.user = await User.findById(decode.id).select('-password')
 
             next()
         } catch(err){
-            console.log(error)
+            console.log(err)
             res.status(401)
-            throw new error('Not authorised')
+            throw new Error('Not authorised')
         }
     }
 
